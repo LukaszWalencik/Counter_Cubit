944f6ebfc8b15d8c1ac69909171663e5a21f4522
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-14 20:59:54 +0200

    Add increment and decrement functions to cubit

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -4,4 +4,12 @@ import 'package:my_counter_cubit/cubits/counter/counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState.initial());
+
+  void increment() {
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  void decrement() {
+    emit(state.copyWith(counter: state.counter - 1));
+  }
 }
